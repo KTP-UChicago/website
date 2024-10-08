9bc89758c0aa5021058b13ae93e4b8d12c32abcf
Nicholas Lee <nicholaslee@uchicago.edu>
2024-10-08 13:14:02 -0500

    adding new banner

---
+++ react-app/src/components/pages/memberDirectoryPage/MemberDirectoryCard.tsx
@@ -0,0 +1,21 @@
+
+interface MemberInfo {
+    name: string,
+    pledgeClass: string,
+    major: string,
+    hometown: string,
+    hobbiesAndInterests: string,
+    comments: string[],
+    pictures: string[],
+    year: number
+    contactInfo: ContactInfo,
+    pastClasses: string[]
+}
+
+interface ContactInfo {
+    email: string,
+    phone: string | undefined,
+    linkedIn: string | undefined,
+    instagram: string | undefined,
+    facebook: string | undefined
+}
