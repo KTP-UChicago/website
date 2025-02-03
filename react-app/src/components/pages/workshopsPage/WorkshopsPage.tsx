725eeb650fa7dcb0ed752caad03e5955c5151845
John <johnruge@uchicago.edu>
2025-02-03 15:59:59 -0600

    updated workshops

--- react-app/src/components/pages/workshopsPage/WorkshopsPage.tsx
+++ react-app/src/components/pages/workshopsPage/WorkshopsPage.tsx
@@ -9,22 +9,22 @@ function WorkshopsPage() {
         script.src = "assets/js/timeline.js";
         script.async = true;
         document.body.appendChild(script);
-        
+
         return () => {
           document.body.removeChild(script);
         };
       }, []);
       useNavigateToId();
-      
+
   return <PageTemplate page="workshops">
          <div>
       <div className="container py-5">
         <div className="scroll" id="apply">
           <div className="row px-3 px-md-0">
-            <h2 className="text-center mb-4">KTP's Unlock Your Tech Potential Winter 2024</h2>
+            <h2 className="text-center mb-4">KTP's Unlock Your Tech Potential Winter 2025</h2>
             <p style={{ textAlign: 'center' }}>
-              We are excited to announce our first annual Unlock Your Tech Potential Workshop Series. You can{' '}
-              <a href="https://docs.google.com/forms/d/e/1FAIpQLSf49ydpVSisujvNGplIfDc4TUpPQTkdlTvmlFQGjT8QBRYbNg/viewform?usp=sf_link">
+              We are excited to announce our second annual Unlock Your Tech Potential Workshop Series. You can{' '}
+              <a href="https://forms.gle/Qy3nddXrFThk4LvPA">
                 RSVP
               </a>{' '}
               here! Join us for an immersive 4-week technology workshop designed to elevate your skills and boost your career in the tech industry.
@@ -43,7 +43,8 @@ function WorkshopsPage() {
               <h2 className="text-center mb-4">Workshop Timeline</h2>
               <p style={{ textAlign: 'center' }}>
                 We will be holding weekly workshops in Stuart 105 starting on 2/8 on Thursdays 7 - 8 PM. If you have any questions,
-                reach out to our Director of Technology, Nicholas (<a href="mailto:nicholaslee@uchicago.edu">nicholaslee@uchicago.edu</a>).
+                reach out to our Co-Directors of Professional Development, Clarice (<a href="mailto:claricek@uchicago.edu">claricek@uchicago.edu</a>)
+                and Arya (<a href="mailto:aryasap@uchicago.edu">aryasap@uchicago.edu</a>).
               </p>
               <div className="cd-timeline js-cd-timeline">
                 <div className="container cd-timeline__container">
@@ -52,13 +53,14 @@ function WorkshopsPage() {
                       <img src="assets/img/workshops/big_little.jpg" alt="Intro Into Tech Recruiting" />
                     </div>
                     <div className="cd-timeline__content">
-                      <h4>Intro Into Tech Recruiting and Linkedin/Resume Review</h4>
+                      <h4>Networking + Intro to Tech Recruiting</h4>
                       <p>
-                        Learn about the basics of recruiting and how to make your resume and LinkedIn stand out during the application process. Get one-on-one feedback
-                        about your resume from our brothers and increase your chances of landing your dream job with personalized advice!
+                      Learn about the basics of tech recruiting and networking to increase your chances of landing your
+                       dream job! We will cover the recruiting timeline for tech internships and provide opportunities
+                       for resume/LinkedIn feedback.
                       </p>
                       <p style={{ fontStyle: 'italic' }}>
-                        Thursday, February 8th | 7:00–8:00 PM | Stuart 105
+                        Monday, February 3rd | 6:00–7:00 PM | Stuart 105
                       </p>
                     </div>
                   </div>
@@ -67,13 +69,14 @@ function WorkshopsPage() {
                       <img src="assets/img/workshops/ellens_fam.jpg" alt="Acing the Technical Interview" />
                     </div>
                     <div className="cd-timeline__content">
-                      <h4>Crash Course on Acing the Technical Interview: Algorithms and Data Structures Overview</h4>
+                      <h4>Algos + DS</h4>
                       <p>
-                        Dive into the fundamentals of algorithms and data structures and learn how to ace the technical interview.
-                        Get the best practices of sharpening your technical skills to help you land your dream job!
+                      Dive into the fundamentals of algorithms and data structures and learn how to ace
+                      the technical interview. Get the best practices of sharpening your technical skills
+                      to help you land your dream job!
                       </p>
                       <p style={{ fontStyle: 'italic' }}>
-                        Thursday, February 15th | 7:00–8:00 PM | Stuart 105
+                        Monday, February 10th | 6:00–7:00 PM | Stuart 105
                       </p>
                     </div>
                   </div>
@@ -82,13 +85,13 @@ function WorkshopsPage() {
                       <img src="assets/img/workshops/medici_table.jpg" alt="Personal Website Design" />
                     </div>
                     <div className="cd-timeline__content">
-                      <h4>Personal Website Design and Portfolio Building</h4>
+                      <h4>PEAK6 Company Recruiting Event</h4>
                       <p>
-                        Craft your own standout personal website with guidance! Learn how to best showcase 
-                        your skills and projects efficiently to impress recruiters and potential employers. 
+                      Come to learn about careers and internship opportunities at a Chicago financial
+                       technology company PEAK6! There will be time for a Q and A with the recruiter.
                       </p>
                       <p style={{ fontStyle: 'italic' }}>
-                        Thursday, February 22nd | 7:00–8:00 PM | Stuart 105
+                        Tuesday, February 18th | 5:00–6:00 PM | Tribune Lounge (2nd Floor Reynolds)
                       </p>
                     </div>
                   </div>
@@ -97,14 +100,14 @@ function WorkshopsPage() {
                       <img src="assets/img/workshops/sf.jpg" alt="Brother Panel: Explore Careers in Tech" />
                     </div>
                     <div className="cd-timeline__content">
-                      <h4>Kappa Theta Pi Brother Panel: Explore Careers in Tech</h4>
+                      <h4>Kappa Theta Pi Brother Panel</h4>
                       <p>
-                        Hear from brothers about their past internship experiences in various tech roles such as 
-                        Data Science, Software Engineering, Product Management and more! We will have experiences 
-                        ranging from Big Tech, Startups, and Finance.
+                      Hear from brothers about their past internship experiences in various tech
+                       roles such as Data Science, Software Engineering, Product Management and more!
+                        We will have experiences ranging from Big Tech, Startups, and Finance.
                       </p>
                       <p style={{ fontStyle: 'italic' }}>
-                        Thursday, February 29th | 7:00–8:00 PM | Stuart 105
+                        Monday, February 28th | 6:00–7:00 PM | Stuart 105
                       </p>
                     </div>
                   </div>
